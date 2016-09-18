7a882d15fcec0c0094b2d9e115c0be173994c9d0
liming <2462384731@qq.com>
2016-09-18 10:31:37 +0800

    Initial Commit

---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by WTCAPPLE_15 on 16/9/18.
+//  Copyright © 2016年 WTCAPPLE_15. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloWorld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by WTCAPPLE_15 on 16/9/18.
+//  Copyright © 2016年 WTCAPPLE_15. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
